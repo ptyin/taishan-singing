02af1d0f2047852c4969de67abb89ab7ea0f2388
PTYin <PeterYin1604@gmail.com>
2019-06-02 01:04:16 +0800

    add time limit control

--- submit.php
+++ submit.php
@@ -1,5 +1,8 @@
 <?php
 include "config.php";
+$file = fopen("started.txt", "r") or die("Unable to open file!");
+$started = boolval(json_decode(readfile($file)));
+fclose($file);
 function guid()
 {
     $code="ABCDEFGHIGKLMNOPQRSTUVWXYZ";
@@ -16,36 +19,49 @@ function guid()
         $f++
         );return $d;
 }
+if(isset($_GET['started'])&&is_bool(json_decode($_GET['started'])))
+{
+    $file = fopen("started.txt", "w");
+    fwrite($file, $_GET['started']);
+}
 if(isset($_COOKIE['once']))
 {
     echo "fail";
 }
 else
 {
-    if(!isset($_GET['singing_vote_id'])||!isset($_GET['reciting_vote_id']))
+    if(!$started)
     {
-        http_response_code(400);
-        echo "error";
+        http_response_code(403);
+        echo "not started";
     }
     else
     {
-        $conn = @mysql_connect(servername, username, password, dbname) or die("连接服务器出错：".mysql_error());
-        @mysql_select_db(dbname) or die("连接数据库出错：".mysql_error());
-        @mysql_query('SET NAMES UTF8');
-        $result1 = mysql_query("update performances set data=data+1 where id=".$_GET['singing_vote_id'].";");
-        $result2 = mysql_query("update performances set data=data+1 where id=".$_GET['reciting_vote_id'].";");
-        if(!$result1||!$result2)
+        if(!isset($_GET['singing_vote_id'])||!isset($_GET['reciting_vote_id']))
         {
             http_response_code(400);
             echo "error";
         }
         else
         {
-            setcookie("once", true, time() + 60 * 60 * 5);
-            echo "success";
+            $conn = @mysql_connect(servername, username, password, dbname) or die("连接服务器出错：".mysql_error());
+            @mysql_select_db(dbname) or die("连接数据库出错：".mysql_error());
+            @mysql_query('SET NAMES UTF8');
+            $result1 = mysql_query("update performances set data=data+1 where id=".$_GET['singing_vote_id'].";");
+            $result2 = mysql_query("update performances set data=data+1 where id=".$_GET['reciting_vote_id'].";");
+            if(!$result1||!$result2)
+            {
+                http_response_code(400);
+                echo "error";
+            }
+            else
+            {
+                setcookie("once", true, time() + 60 * 60 * 5);
+                echo "success";
+            }
+            mysql_free_result($result1);
+            mysql_free_result($result2);
+            mysql_close($conn);
         }
-        mysql_free_result($result1);
-        mysql_free_result($result2);
-        mysql_close($conn);
     }
 }
